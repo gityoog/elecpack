b8c65eb5d03fe8c5eebed44fbe2ff1e463238269
Your Name <you@example.com>
2024-09-23 10:54:50 +0800

    Refactor MainConfig to include assets path in the configuration

--- src/config/main/index.ts
+++ src/config/main/index.ts
@@ -105,7 +105,7 @@ class MainConfig {
             renderer,
             files,
             assets: ${JSON.stringify(this.assets !== undefined)} ? 
-              path.resolve(root, ${JSON.stringify(relative)}, 'assets')
+              path.resolve(root, ${JSON.stringify(relative)}, ${MainConfig.MAIN}, 'assets')
             : undefined
           })
         })()`
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import ElecpackBuilder from "./builder"
 import type WebpackBuilder from "./common/webpack-builder"
+import Config from "./config"
 
 type ElecpackConfigFile = WebpackBuilder.ConfigFile
+type Configuration = Config.Options
 
 export {
   ElecpackBuilder,
   ElecpackConfigFile,
+  Configuration
 }
